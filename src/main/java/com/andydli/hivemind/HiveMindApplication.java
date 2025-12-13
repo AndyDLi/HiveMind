a06f446d744c0dcfdfeb1e365e138b4ffe8c666e
Andy Li <andy.dang.li@gmail.com>
2025-12-12 19:48:31 -0500

    Initial Commit

---
+++ src/main/java/com/andydli/hivemind/HiveMindApplication.java
@@ -0,0 +1,13 @@
+package com.andydli.hivemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andydli/hivemind/HiveMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andydli.hivemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
