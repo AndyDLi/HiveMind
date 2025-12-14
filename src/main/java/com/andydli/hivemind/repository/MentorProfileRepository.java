3ab43afe9e33e07a3e5a2a8e582df5ae99a7a0a5
Andy Li <andy.dang.li@gmail.com>
2025-12-13 21:23:43 -0500

    Removed Unused Import

--- src/main/java/com/andydli/hivemind/repository/MentorProfileRepository.java
+++ src/main/java/com/andydli/hivemind/repository/MentorProfileRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.andydli.hivemind.model.MentorProfile;
 
 import java.util.Optional;
-import java.util.List;
 
 public interface MentorProfileRepository extends JpaRepository<MentorProfile, Long> {
     Optional<MentorProfile> findByUser_Id(Long userId);
