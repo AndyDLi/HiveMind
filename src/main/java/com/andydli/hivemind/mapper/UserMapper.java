e1156945e56c1d7f1c07d70611b950a841fb04d0
Andy Li <andy.dang.li@gmail.com>
2025-12-15 10:31:54 -0500

    Created UserMapper and Modified registerUser Using Mapper

---
+++ src/main/java/com/andydli/hivemind/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.andydli.hivemind.mapper;
+
+import org.springframework.stereotype.Component;
+import com.andydli.hivemind.model.User;
+import com.andydli.hivemind.dto.UserDTO;
+import com.andydli.hivemind.dto.UserRegistrationDTO;
+
+@Component
+public class UserMapper {
+    public UserDTO userToUserDTO(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        return new UserDTO(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
+    public User toEntity(UserRegistrationDTO userRegistrationDTO) {
+        if (userRegistrationDTO == null) {
+            return null;
+        }
+
+        User user = new User();
+        user.setEmail(userRegistrationDTO.email());
+        user.setFirstName(userRegistrationDTO.firstName());
+        user.setLastName(userRegistrationDTO.lastName());
+        return user;
+    }
+}
