517d01ecca9b059e4ecd84819406ab8c02ff4090
Andy Li <andy.dang.li@gmail.com>
2025-12-24 00:50:03 -0500

    Added PortalController for createPortal

---
+++ src/main/java/com/andydli/hivemind/controller/PortalController.java
@@ -0,0 +1,31 @@
+package com.andydli.hivemind.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.andydli.hivemind.service.PortalService;
+import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
+import com.andydli.hivemind.dto.PortalDTO;
+import com.andydli.hivemind.dto.PortalCreationDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.andydli.hivemind.model.User;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+@RequestMapping("/api/portal")
+public class PortalController {
+    private final PortalService portalService;
+
+    public PortalController(PortalService portalService) {
+        this.portalService = portalService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PortalDTO> createPortal(
+            @Valid @RequestBody PortalCreationDTO portalCreationDTO,
+            @AuthenticationPrincipal User user
+    ) {
+        PortalDTO portalDTO = portalService.createPortal(portalCreationDTO, user.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(portalDTO);
+    }
+}
